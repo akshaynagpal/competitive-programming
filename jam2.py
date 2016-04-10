d4062201b8437879322560cfccde6a0ae713f2db
Akshay Nagpal <akshay2626@gmail.com>
2016-04-10 14:43:05 +0530

    codejam try

---
+++ jam2.py
@@ -0,0 +1,12 @@
+T = int(raw_input())
+input_list = []
+for i in range(T):
+	input_list.append(raw_input())
+
+
+
+# set1 = set('++++') 
+# print set1
+# if '+' in set1 and len(set1)==1:
+# 	print 'happy'
+# print len(set1)
---
+++ jam3.py
@@ -0,0 +1,3 @@
+t,n,j = raw_input().split()
+
+print t,n,j
